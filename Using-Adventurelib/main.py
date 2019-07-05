b9e6494e8e7e017873d7bad6ebe4d55d411af734
Darrell Little <darrell.little@gmail.com>
2019-07-04 22:18:42 -0400

    Added Characters and Item properties

--- Using-Adventurelib/main.py
+++ Using-Adventurelib/main.py
@@ -14,7 +14,7 @@ placed into the player's inventory
 # Place the adventurelib.py file in the root folder and import
 from adventurelib import *
 
-# The remaining logic of the game goes before the start() call
+# The remaining logic of the game goes before calling the start() function
 
 # A Bag() is an class used to hold a collection of items
 # Create an instance of Bag() for the player's inventory
@@ -26,16 +26,34 @@ Room.items = Bag()
 # Create the items to be found in the game
 # Use the Item() class to create objects for the player to find
 key = Item('small key','key')
+key.carry = True
 sword = Item('broad sword','sword')
-flower = Item('pot of flowers','flowers')
+sword.carry = True
+flower = Item('pot of flowers','flower','flowers')
+flower.def_name = 'the pot of flowers'
+flower.carry = False
 potion = Item('magic potion','potion')
+potion.carry = True
+cheese = Item('block of cheese', 'cheese')
+cheese.carry = True
+# Also create characters in the game using the Item() class
+# Add properties to the character such as health level and weakness in battle
+greenZombie = Item('green Zombie','zombie')
+greenZombie.def_name = 'the green Zombie'
+greenZombie.carry = False
+greenZombie.health = 3
+greenZombie.weakness = 'sword'
 
+redZombie = Item('red Zombie','zombie')
+redZombie.def_name = 'the red Zombie'
+redZombie.carry = False
+redZombie.health = 3
+redZombie.weakness = 'cheese'
 
 # Start the game with the player in the Hall using the class Room() 
 current_room = Hall = Room(
     """
     You are in the main downstairs hallway.
-    You see exits in every direction.
     """
 )
 # Place items as a property of a room
@@ -46,67 +64,59 @@ Kitchen = Hall.south = Room(
     """
     A large, very clean kitchen. 
     There should be flowers here for decoration.
-    There are exits to the north, west and east.
     """
 )
-
-Kitchen.items = Bag({flower,})
+Kitchen.items = Bag({flower,redZombie})
 
 Dining_Room = Hall.east = Room(
     """
     You are in the dining room. There is a long, majestic
     mahagony table in the center of the room.
-    There are exits to the west, south and north.
     """
 )
-
-Dining_Room.items = Bag({sword,})
+Dining_Room.items = Bag({sword,cheese})
 
 Library = Hall.west = Room(
     """
-    You are in the library.
-    An impressive collection of very old books.
-    There are exits to the east, south and north.
+    You are in the library with
+    an impressive collection of very old books.
     """
 )
-
 Library.items = Bag({potion,})
 
 Study = Library.south = Room(
     """
     This is the Study.
     A quiet room with large, comfortable chairs.
-    There are exits to the north and east.
     """
 )
+Study.items = Bag({greenZombie,})
 
 Parlor = Library.north = Room(
     """
     A parlor nicely decorated with paintings and 
     number of chairs arranged in a circle.
-    Therer is an exit to the south and east.
     """
 )
 
 Atrium = Hall.north = Room(
     """
     An atrium with a high, vaulted ceiling.
-    There are exits in every direction.
     """
 )
 
 Garden = Atrium.north = Room(
     """
-    A magnificent, sprawling garden with every 
-    imaginable type of flower, shrubbery and trees.
-    The entrance to the house is to the south.
+    You are outside of the house in
+    a magnificent, sprawling garden with every 
+    imaginable type of flower, shrubbery and 
+    a number of huge trees.
     """
 )
 
 Guest_Bedroom = Dining_Room.north = Room(
     """
     A small, cozy guest bedroom with a window looking out on the Garden.
-    There are exits to the south and west.
     """
 )
 
@@ -114,7 +124,6 @@ Pantry = Dining_Room.south = Room(
     """
     A pantry with rows of shelves containing kitchen utensils 
     and an ample supply of canned goods.
-    There exits to the north and west.
     """
 )
 
@@ -140,11 +149,16 @@ def go(direction):
         look()
 
 @when('take ITEM')
+@when('get ITEM')
 def take(item):
-    obj = current_room.items.take(item)
+    obj = current_room.items.find(item)
     if obj:
-        say(f'You pick up the {obj}.')
-        inventory.add(obj)
+        if obj.carry:
+            obj = current_room.items.take(item)
+            say(f'You pick up the {obj}.')
+            inventory.add(obj)
+        else:
+            say(f'You can not hold {obj.def_name}')
     else:
         say(f'There is no {item} here.')
 
@@ -163,6 +177,11 @@ def look():
     if current_room.items:
         for i in current_room.items:
             say(f'A {i} is here.')
+
+@when('doors')
+def list_exits():
+    for exits in current_room.exits():
+        say(f'There is a door to the {exits}.')
 
 @when('inventory')
 def show_inventory():
